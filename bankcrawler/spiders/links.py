74a7abab62f788a74184ce64c0cb09043760a723
Jin Zou <zou.mengguang@gmail.com>
2019-12-03 16:47:58 -0800

    Change settings
    # Additional explanation and summary here, if necessary. (72 chars.)
    # Always prepend the start of the message with the following:
    This commit will 
    
    # - Use hyphens to write out a list
    # - That will break down the commit
    # - If necessary
    
    # Refs: #12 (Use this format for referencing a GitHub Issue)

--- bankcrawler/spiders/links.py
+++ bankcrawler/spiders/links.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from urlparse import urlparse
 
+# set to track visited domains
+url_set = set()
 
 class LinksSpider(scrapy.Spider):
     name = 'links'
-    allowed_domains = ['unitedbanks.com']
-    start_urls = ['http://unitedbanks.com/']
+    allowed_domains = ['unitedbank.com']
+    start_urls = ['http://www.unitedbank.com']
 
     def parse(self, response):
-        pass
+        for href in response.xpath('//a/@href').getall():
+            if href.startswith('http') and 'unitedbank.com' not in href:
+                domain = urlparse(href).netloc
+                if domain and domain not in url_set:
+                    url_set.add(domain)
+                    yield { 'url': domain }
+            yield scrapy.Request(response.urljoin(href), self.parse)
