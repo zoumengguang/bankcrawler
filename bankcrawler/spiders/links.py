646fbb8bd60e230014d006749febf667797e7b40
Jin Zou <zou.mengguang@gmail.com>
2019-12-05 16:06:47 -0800

    Basic crawling capability for needed fields
    Known issue(s): duplicate path visitation

--- bankcrawler/spiders/links.py
+++ bankcrawler/spiders/links.py
@@ -1,30 +1,39 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # Spider takes input as .csv with env var to local file
-import scrapy, os, csv
-""" import pprint """
+import scrapy
+import os
+import csv
+import re
+import pprint
 from urlparse import urlparse
-from bankcrawler.items import BankcrawlerItem
 
 LOCAL_DATA_PATH = os.getenv('LOCAL_DATA_PATH')
 bankDict = {}
 bankUrls = []
 bankDomains = []
 
-""" pp = pprint.PrettyPrinter(indent=4) """
+
+pp = pprint.PrettyPrinter(indent=4)
 
 # Parse .csv and populate dict/lists
 reader = csv.DictReader(open(LOCAL_DATA_PATH))
 for row in reader:
     values = row.values()
-    bankDict[int(values[3])] = { "Bank Domain": values[0], "IDRSSD": values[1], "Bank Name": values[2], "ABA Routing Number": values[4] }
-    bankUrls.append(values[0])
-    bankDomains.append(values[0].replace('http://','').replace('https://','').replace('/',''))
-    """ pp.pprint(bankDict[int(values[3])]) """
+    bankDomain = values[0].replace(
+        'http://', '').replace('https://', '').replace('/', '')
+    bankUrl = 'http://' + \
+        values[0] if not re.search('^https?://', values[0]) else values[0]
+    bankDict[bankDomain] = {
+        "FDIC Cert": values[3],
+        "Bank URL": bankUrl,
+        "IDRSSD": values[1],
+        "Bank Name": values[2],
+        "ABA Routing Number": values[4]
+    }
+    bankUrls.append(bankUrl)
+    bankDomains.append(bankDomain)
+    """ pp.pprint(bankDict[bankDomain]) """
 
-""" print(bankUrls)
-print(bankDomains) """
-
-url_set = set()
 
 class LinksSpider(scrapy.Spider):
     name = 'links'
@@ -33,19 +42,26 @@ class LinksSpider(scrapy.Spider):
     """     allowed_domains = bankDomains
     start_urls = bankUrls """
 
+    # print(response.request) Response type (GET, POST) and url
+    # print(response.status) HTTP status code
+    # print(response.request.meta.get('redirect_urls')) get all followed redirect urls to get to current path
+    # print(response.meta['start_url']) get originating start_url of current url
     def parse(self, response):
-        # print(response.request) Response type and url
-        # print(response.status) HTTP status code
-        # print(response.request.meta.get('redirect_urls')) get all followed redirect urls to get to current path
-        print(response.meta['start_url'])
         for href in response.xpath('//a/@href').getall():
-            """ item = BankcrawlerItem() """
-            """ if href.startswith('http'): """
-            if href.startswith('http') and 'unitedbank.com' not in href:
-                domain = urlparse(href).netloc
-                """ item["domain"] = urlparse(href).netloc
-                item["path"] = urlparse(href).path """
-                if domain and domain not in url_set:
-                    url_set.add(domain)
-                    yield { "url": domain }
+            if href.startswith('http'):
+                curDomain = urlparse(href).netloc
+                curPath = urlparse(href).path
+                if curDomain:
+                    curUrlOrgStart = response.meta['start_url'].replace(
+                        'http://', '').replace('https://', '').replace('/', '')
+                    curBankInfo = bankDict[curUrlOrgStart]
+                    linkType = 'Internal' if curDomain in curUrlOrgStart else 'External'
+                    yield {
+                        'FDIC Cert': curBankInfo['FDIC Cert'],
+                        'Bank Domain': curUrlOrgStart,
+                        'Link Type': linkType,
+                        'Link Domain': curDomain,
+                        'Link Path': curPath,
+                        'Link Status Response': response.status
+                    }
             yield scrapy.Request(response.urljoin(href), self.parse)
